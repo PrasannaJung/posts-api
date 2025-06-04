b0d960bb578466d1f58f55da9b6fc6250d0eb7ac
PrasannaJung <prasannajung08@gmail.com>
2025-06-04 16:47:00 +0545

    Extended and adjusted the Request type in express

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,21 @@
 import express, { Request, Response } from "express";
 import postRoutes from "./routes/post.routes";
 import { router as userRoutes } from "./routes/user.routes";
+import { router as reviewRoutes } from "./routes/review.routes";
 import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/test", (req: Request, res: Response) => {
-  res.end("REQUEST RECEIVED SUCCESSFULLY");
-});
-
 app.use("/api/v1/posts", postRoutes);
 app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/reviews", reviewRoutes);
 
 app.use(errorHandler);
 
 export { app };
+
+// app.get("/test", (req: Request, res: Response) => {
+//   res.end("REQUEST RECEIVED SUCCESSFULLY");
+// });
--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -3,7 +3,12 @@ import express from "express";
 declare global {
   namespace Express {
     interface Request {
-      user?: Record<string, any>;
+      user?: {
+        username: string;
+        id: string;
+        iat: number;
+        exp: number;
+      };
     }
   }
 }
